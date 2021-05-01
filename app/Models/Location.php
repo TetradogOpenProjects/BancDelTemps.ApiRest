5ab222c013e9969b09a149b8836b2681b9e3019a
Gabriel Cortés Garcia <tetradog@gmail.com>
2021-05-01 11:01:57 +0200

    commit

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -7,5 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Location extends Model
 {
     use HasFactory;
-    //
+    protected $fillable = [
+        'name',
+        'address',
+        'latitude',
+        'longitude',
+    ];
 }
